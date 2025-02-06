dbcd71feb8bc9bcde0419fe7376e5a5de68c021a
Meylie <emilie.desgranges78@gmail.com>
2025-02-06 09:58:11 +0100

    Ajout base de code du blog
    A modifier

--- resa_web/view/connexion.php
+++ resa_web/view/connexion.php
@@ -1,5 +1,26 @@
 <main>
+<form method="POST">
+    <!-- action="view/traite_login.php" -->
+    <?php if (isset($msg) && $msg == "inscription") {
+        echo "Votre compte à bien été créé <br>";
+    } ?>
+    <label for="login">Login</label>
+    <input type="text" name="login" id="login" required>
+    <?php if (isset($msg) && $msg == "err-login") {
+        echo "Mauvais login";
+    } ?>
 
+    <label for="mdp">Mot de passe</label>
+    <input type="text" name="mdp" id="mdp" required>
+    <?php if (isset($msg) && $msg == "err-mdp") {
+        echo "Mauvais mot de passe";
+    } ?>
+
+    <input type="submit">
+
+</form>
+
+<a class="txt-center" href="index.php?action=inscription">Vous n'avez pas de compte ? Inscrivez-vous</a>
 
 
 
--- resa_web/view/header.php
+++ resa_web/view/header.php
@@ -1,3 +1,9 @@
 <header>
-    
+    <nav>
+        <ul>
+            <li><a href="index.php?action=connexion">connexion</a></li>
+            <li><a href="index.php?action=inscription">inscription</a></li>
+            <li><a href="index.php?action=deconnexion">deconnexion</a></li>
+        </ul>
+    </nav>
 </header>

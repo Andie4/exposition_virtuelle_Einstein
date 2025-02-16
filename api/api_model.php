1eba0c24b416274fb3782c83466f15d3fb6da5d5
Emilie.D <152283886+dragondoee@users.noreply.github.com>
2025-02-16 18:42:22 +0100

    Skeleton API GET + MODEL

---
+++ api/api_model.php
@@ -0,0 +1,132 @@
+<?php
+
+
+// ===============================================================  GET  ============================================================================
+
+// GET USER
+function getOneUser($id)
+{
+
+}
+;
+
+function getAllUser()
+{
+}
+;
+
+// GET RESA
+function getOneResa($id)
+{
+}
+;
+
+function getAllResa()
+{
+}
+;
+
+// GET BILLET
+function getOneBillet($id)
+{
+}
+;
+
+function getAllBillet()
+{
+}
+;
+
+// GET TARIF
+function getOneTarif($id)
+{
+}
+;
+
+function getAllTarif()
+{
+}
+;
+
+// ===============================================================  POST  ============================================================================
+
+// POST USER
+function postUser()
+{
+
+}
+;
+
+// POST RESA
+function postResa()
+{
+}
+;
+
+// POST BILLET
+function postBillet()
+{
+}
+;
+
+// POST TARIF
+function postTarif()
+{
+}
+;
+
+
+// ===============================================================  PUT  ============================================================================
+
+// PUT USER
+function putUser()
+{
+
+}
+;
+
+// PUT RESA
+function putResa()
+{
+}
+;
+
+// PUT BILLET
+function putBillet()
+{
+}
+;
+
+// PUT TARIF
+function putTarif()
+{
+}
+;
+
+
+// ===============================================================  DELETE  ============================================================================
+
+// DELETE USER
+function deleteUser()
+{
+
+}
+;
+
+// DELETE RESA
+function deleteResa()
+{
+}
+;
+
+// DELETE BILLET
+function deleteBillet()
+{
+}
+;
+
+// DELETE TARIF
+function deleteTarif()
+{
+}
+;
--- resa_web/index.php
+++ resa_web/index.php
@@ -6,18 +6,84 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<?php
+    <?php
+    include "api_model.php";
+    $request_method = $_SERVER["REQUEST_METHOD"];
 
-if (isset($_GET["action"])) {
-    $action = $_GET["action"];
-
-    switch ($action) {
-        
+    switch ($request_method) {
+        case 'GET':
+            switch ($type) {
+                case 'user':
+                    if (isset($_GET["id"])){
+                        $result=getOneUser($_GET["id"]);
+                    } else {
+                        $result=getAllUser();
+                    }
+                    break;
+                case 'resa':
+                    if (isset($_GET["id"])){
+                        $result=getOneResa($_GET["id"]);
+                    } else {
+                        $result=getAllResa();
+                    }
+                    break;
+                case 'billet':
+                    if (isset($_GET["id"])){
+                        $result=getOneBillet($_GET["id"]);
+                    } else {
+                        $result=getAllBillet();
+                    }
+                    break;
+                case 'tarif':
+                    if (isset($_GET["id"])){
+                        $result=getOneTarif($_GET["id"]);
+                    } else {
+                        $result=getAllTarif();
+                    }
+                    break;
+            }
+            break;
+        case 'POST':
+            switch ($type) {
+                case 'user':
+                    break;
+                case 'resa':
+                    break;
+                case 'billet':
+                    break;
+                case 'tarif':
+                    break;
+            }
+            break;
+        case 'PUT':
+            switch ($type) {
+                case 'user':
+                    break;
+                case 'resa':
+                    break;
+                case 'billet':
+                    break;
+                case 'tarif':
+                    break;
+            }
+            break;
+        case 'DELETE':
+            switch ($type) {
+                case 'user':
+                    break;
+                case 'resa':
+                    break;
+                case 'billet':
+                    break;
+                case 'tarif':
+                    break;
+            }
+            break;
     }
-} else {
-    require "view/default.php";
-}
-?>
+
+    ?>
 </body>
+
 </html>

285e7d08a10fe5a618793686b388e11ed4b55788
Emilie.D <152283886+dragondoee@users.noreply.github.com>
2025-02-16 18:08:51 +0100

    delete resaweb

--- resa_web/index.php
+++ resa_web/index.php
@@ -1,12 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<?php
-require "model.php";
-require "view/header.php";
-require "view/footer.php";
-?>
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,25 +13,7 @@ if (isset($_GET["action"])) {
     $action = $_GET["action"];
 
     switch ($action) {
-        // Connexion
-        case "connexion":
-            if (isset($_POST["login"])) {
-                $msg = login($_POST);
-            }
-            require "view/connexion.php";
-            break;
-        // Inscription
-        case "inscription":
-            if (isset($_POST["login"])) {
-                $msg = inscription($_POST);
-            }
-            require "view/inscription.php";
-            break;
-        // deconnexion
-        case "deconnexion":
-            deconnexion();
-            break;
-
+        
     }
 } else {
     require "view/default.php";

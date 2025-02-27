4af1a8967c98bfaa8cffc67d4e4b7e12d317ecbf
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-02-27 01:28:15 +0100

    design du formulaire
    Le design est partiellement terminé. Il faut que je corrige mon code js concernant le compte du nombre de billet ainsi que leur ajout et leur suppression. J'ai créer 3 formulaire différents il faut que je les transform en un seule formulaire diviser en plusieurs fielsets.

--- formulaire.php
+++ formulaire.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles/formulaire.css">
+    <script src="scripts/formulaire.js" defer></script>
     <title>Réservation</title>
 </head>
 <body>
@@ -22,7 +23,15 @@
     </header>
 
         <section>
-        <h1>Acheter un billet</h1>
+        <div>
+            <h1>Acheter un billet</h1>
+            <!-- <div class="filAriane">
+                <button>Dates & horaires ></button>
+                <button>Nombre de billets ></button>
+                <button>Coordonnées</button>
+            </div> -->
+        </div>
+        
 
         <!-- Dates & horaires  -->
         <form action="">
@@ -40,14 +49,14 @@
                 <div class="bigMarginBottom">
                     <label>Choisissez un horaire <span class="red">*</span></label>
                     <div class="buttons">
-                        <button>10:00</button>
-                        <button>11:00</button>
-                        <button>12:00</button>
-                        <button>14:00</button>
-                        <button>15:00</button>
-                        <button>16:00</button>
-                        <button>17:00</button>
-                        <button>18:00</button>
+                        <button >10:00</button>
+                        <button >11:00</button>
+                        <button >12:00</button>
+                        <button >14:00</button>
+                        <button >15:00</button>
+                        <button >16:00</button>
+                        <button >17:00</button>
+                        <button >18:00</button>
                     </div>
                 </div>
                 
@@ -63,21 +72,115 @@
                     <p>Mode d'obtention : e-ticket (gratuit)</p>
                 </div>
             </div>
-            
-
         </div>
 
 
 
         <!-- Nombre de billets -->
         <form action="">
+            <div class="container">
+                <div class="bigMarginBottom">
+                    <h2 class="tinyMarginBottom">Nombre de billets</h2>
+                    <p class="minuscule tinyMarginTop">Les champs suivis d'un <span class="red">*</span> sont obligatoires.</p>
+                </div>
+                <div>
+                <div class="">
+        <div class="ticket-category">
+            <div class="flex">
+                <label class="tarif">Plein tarif</label>
+                <div class="counter">
+                    <button class="supprimer">-</button>
+                    <span>0</span>
+                    <button class="ajouter">+</button>
+                </div>
+            </div>
+            <!-- <div class="details">
+                <div class="person">
+                    <label class="tarif">Plein tarif 1</label>
+                    <input type="text" placeholder="Nom">
+                    <input type="text" placeholder="Prénom">
+                </div>
 
+            </div> -->
+        </div>
+        
+        <div class="ticket-category">
+            <div class="flex">
+                <label class="tarif">Enfant -16 ans</label>
+                <div class="counter">
+                    <button class="supprimer">-</button>
+                    <span>0</span>
+                    <button class="ajouter">+</button>
+                </div>
+            </div>
+            
+        </div>
+            
+        </div>
+        
+        <div class="ticket-category">
+            <div class="flex">
+                <label class="tarif">Jeune -26 ans</label>
+                <div class="counter">
+                    <button class="supprimer">-</button>
+                    <span>0</span>
+                    <button class="ajouter">+</button>
+                </div>
+            </div>
+            
+        </div>
+        
+        <div class="ticket-category">
+            <div class="flex">
+                <label class="tarif">Sénior +65 ans</label>
+                <div class="counter">
+                    <button class="supprimer">-</button>
+                    <span>0</span>
+                    <button class="ajouter">+</button>
+                </div>
+            </div>
+            
+        </div>
+                
+                <button class="validate">Valider</button>
+            </div>
         </form>
+
+        <div class="ticket">
+            <div class="pointilles">
+                <img src="images/img_ticket.jpg" alt="">
+                <div class="ticketInfos">
+                    <p>Lorem ipsum dolor sit</p>
+                    <p>Mode d'obtention : e-ticket (gratuit)</p>
+                </div>
+            </div>
+        </div>
+
 
 
 
         <!-- Coordonnées -->
         <form action="">
+            <div class="container">
+                <div class="bigMarginBottom">
+                    <h2 class="tinyMarginBottom">Coordonnées</h2>
+                    <p class="minuscule tinyMarginTop">Les champs suivis d'un <span class="red">*</span> sont obligatoires.</p>
+                </div>
+                <div>
+                    <label for="nom">Nom <span class="red">*</span></label><br>
+                    <input type="text" id="nom" class="nom" required><br>
+                </div>
+                <div>
+                    <label for="prenom">Prénom <span class="red">*</span></label><br>
+                    <input type="text" id="prenom" class="prenom" required><br>
+                </div>
+                <div>
+                    <label for="email">Email <span class="red">*</span></label><br>
+                    <input type="email" id="email" class="email" required><br>
+                </div>
+
+                <button class="validate">Valider</button>
+
 
         </form>
 

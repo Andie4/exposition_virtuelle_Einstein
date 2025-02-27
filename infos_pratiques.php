09e5a0ed60c3fce3238a4cc876e7241ede24512a
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-02-27 13:22:03 +0100

    front de la page infos pratiques
    version mobile/tablet et desktop

--- infos_pratiques.php
+++ infos_pratiques.php
@@ -23,48 +23,47 @@
 
     <section>
         <h1>Informations pratiques</h1>
-       <div>
-            <img src="" alt="">
-            <div>
+       <div class="spaceBetweenText box bigMarginBottom">
+            <img src="images/img_planSalles.png" alt="">
+            <div class="bigMarginBottom marginLeft">
                 <h2>Nom de la salle</h2>
-                <p>Lorem ipsum dolor sit amet. Qui omnis quasi qui corrupti quod in veniam cupiditate ut labore accusamus At odit officia ut voluptatem soluta aut maxime excepturi. Ut dolor minus ut dolore totam et consequatur dicta est error error. Rem voluptatum provident sit dicta earum in porro mollitia aut nihil voluptates. Et sunt eveniet id quia quos ut modi molestiae.</p>
+                <p class="texte">Lorem ipsum dolor sit amet. Qui omnis quasi qui corrupti quod in veniam cupiditate ut labore accusamus At odit officia ut voluptatem soluta aut maxime excepturi. Ut dolor minus ut dolore totam et consequatur dicta est error error. Rem voluptatum provident sit dicta earum in porro mollitia aut nihil voluptates. Et sunt eveniet id quia quos ut modi molestiae.</p>
             </div>
        </div>
 
-       <div>
-            <div>
-                <div>
-                    <h2>HORAIRES</h2>
-                    <P>L’exposition est ouverte du lundi au dimanche de 10h à 19h. La dernière entrée est à 18h.</P>
-                    <p>La boutique de l’exposition est ouverte du lundi au dimanche de 10h à 19h.</p>
+       <div class="flex ">
+            <div class=" box">
+                <div class="bigMarginBottom ">
+                    <h2 class="tinyMarginBottom">HORAIRES</h2>
+                    <P>L’exposition est ouverte du lundi au dimanche de 10h à 19h. La dernière entrée est à 18h. <br> La boutique de l’exposition est ouverte du lundi au dimanche de 10h à 19h.</p>
                 </div>
-                <div>
-                    <h2>TRANSPORTS</h2>
+                <div class="bigMarginBottom ">
+                    <h2 class="tinyMarginBottom">TRANSPORTS</h2>
                     <p>Train : RER A - Arrêt Noisy-champsBus : lignes 312, 213, 212</p>
                 </div>
-                <div>
-                    <h2>TARIFS</h2>
+                <div class="bigMarginBottom">
+                    <h2 class="tinyMarginBottom">TARIFS</h2>
                     <p>Plein tarif : 0€ / Tarif réduit : 0€</p>
                 </div>
 
-                <div>
-                    <h2>ACCÈS</h2>
+                <div class="bigMarginBottom">
+                    <h2 class="tinyMarginBottom">ACCÈS</h2>
                     <p>2 Rue Albert Einstein</p>
                     <p>77420 Champs-sur-Marne</p> 
                     <p>Téléphone : +33123456789</p>
                     <p>Mail : exporelatix@gmail.com</p>
                 </div>
-                <div>
-                    <h2>ACCESSIBILITÉ</h2>
+                <div class="bigMarginBottom">
+                    <h2 class="tinyMarginBottom">ACCESSIBILITÉ</h2>
                     <p>L’exposition est accessible aux personnes à mobilité réduite.</p>
                 </div>
-                <div>
-                    <h2>RÉSEAU WI-FI GRATUIT</h2>
+                <div class="bigMarginBottom">
+                    <h2 class="tinyMarginBottom">RÉSEAU WI-FI GRATUIT</h2>
                     <p>L’exposition dispose d'un réseau wifi gratuit et ouvert à tous. Pour se connecter, sélectionner le réseau "Relatix_Wifi" et accepter les conditions générales d'utilisation. Vous serez redirigés vers le site du musée et vous pourrez naviguer librement dans les zones du musée couvertes par le réseau.</p>
                 </div>
             </div>
-            <div>
-                <img src="" alt="">
+            <div class="centerH">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2626.103289310843!2d2.582737976696182!3d48.837168471329186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e60e3161ea094f%3A0x1eac03d42a240dc3!2s2%20Rue%20Albert%20Einstein%2C%2077420%20Champs-sur-Marne!5e0!3m2!1sfr!2sfr!4v1740656428084!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="marginLeft"></iframe>
             </div>
             
        </div>

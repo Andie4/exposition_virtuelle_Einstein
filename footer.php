d169476225f25a9423a00a1419e1d110ad14d5ae
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-02-27 14:40:19 +0100

    ajout des lien vers les réseaux sociaux de l'angence

--- footer.php
+++ footer.php
@@ -18,24 +18,24 @@
                     <p>RelatiX</p>
                 </div>
                 <div class="spaceFooter">
-                    <p>Accueil</p>
-                    <p>Infos pratiques</p>
-                    <p>Réserver un billet</p>
+                    <a href="index.php">Accueil</a>
+                    <a href="infos_pratiques.php">Infos pratiques</a>
+                    <a href="formulaire.php">Réserver un billet</a>
                 </div>
                 <div class="spaceFooter">
                     <p>Agence Xploria</p>
                     <p>2 Rue Albert Einstein</p>
                     <p>77420 Champs-sur-Marne</p>
                     <div>
-                        <a href=""><img src="" alt=""></a>
-                        <a href=""><img src="" alt=""></a>
-                        <a href=""><img src="" alt=""></a>
+                        <a href="https://www.instagram.com/xploria_agency?igsh=MWo3Nm1mNHhsM2Y0OA==" target="_blank"><img src="images/instagram.svg" alt="" class="icone"></a>
+                        <a href="https://www.tiktok.com/@xploria_agency?_t=ZN-8uGYZ05NkbG&_r=1" target="_blank"><img src="images/tiktok.svg" alt="" class="icone"></a>
+                        <a href="" target="_blank"><img src="images/youtube.svg" alt="" class="icone"></a>
                     </div>
                 </div>
             </div>
             <div class="flex">
-                <p>Conditions Générales de vente</p>
-                <p>Mentions Légales</p>
+                <a href="CGV.php"><img src="" alt="">Conditions Générales de vente</a>
+                <a href="mentions_legales.php"><img src="" alt="">Mentions Légales</a>
                 <p>Ouvert tous les jours de 10h à 19h</p>
                 <p>© 2020 RelatiX. Tous droits réservés.</p>
             </div>

87640d3ce54d49b7664310d403eb7893a364fc9b
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-02-27 20:37:07 +0100

    formulaire version tablet et desktop

--- formulaire.php
+++ formulaire.php
@@ -23,155 +23,185 @@
         
 
         <!-- Dates & horaires  -->
-        <form action="">
-            <div class="container">
-                <div class="bigMarginBottom">
-                    <h2 class="tinyMarginBottom">Dates & horaires</h2>
-                    <p class="minuscule tinyMarginTop">Les champs suivis d'un <span class="red">*</span> sont obligatoires.</p>
-                </div>
 
-                <div class="bigMarginBottom">
-                    <label for="date">Choisissez une date <span class="red">*</span></label><br>
-                    <input type="date" id="date" class="date" required><br>
-                </div>
 
-                <div class="bigMarginBottom">
-                    <label>Choisissez un horaire <span class="red">*</span></label>
-                    <div class="buttons">
-                        <button >10:00</button>
-                        <button >11:00</button>
-                        <button >12:00</button>
-                        <button >14:00</button>
-                        <button >15:00</button>
-                        <button >16:00</button>
-                        <button >17:00</button>
-                        <button >18:00</button>
+        <div class="column">
+                <form action="">
+                <div class="container">
+                    <div class="bigMarginBottom">
+                        <h2 class="tinyMarginBottom">Dates & horaires</h2>
+                        <p class="minuscule tinyMarginTop">Les champs suivis d'un <span class="red">*</span> sont obligatoires.</p>
+                    </div>
+
+                    <div class="bigMarginBottom">
+                        <label for="date">Choisissez une date <span class="red">*</span></label><br>
+                        <input type="date" id="date" class="date" required><br>
+                    </div>
+
+                    <div class="bigMarginBottom">
+                        <label>Choisissez un horaire <span class="red">*</span></label>
+                        <div class="buttons">
+                            <button >10:00</button>
+                            <button >11:00</button>
+                            <button >12:00</button>
+                            <button >14:00</button>
+                            <button >15:00</button>
+                            <button >16:00</button>
+                            <button >17:00</button>
+                            <button >18:00</button>
+                        </div>
+                    </div>
+                    
+                    <button class="validate">Valider</button>
+                </div>
+                    </form>
+                    
+                    <div class="ticket">
+                <div class="pointilles">
+                    <img src="images/img_ticket.jpg" alt="" class="ticketImg">
+                    <div class="ticketInfos">
+                        <p>Lorem ipsum dolor sit</p>
+                        <p>Mode d'obtention : e-ticket (gratuit)</p>
                     </div>
                 </div>
-                
-                <button class="validate">Valider</button>
             </div>
-        </form>
-
-        <div class="ticket">
-            <div class="pointilles">
-                <img src="images/img_ticket.jpg" alt="">
-                <div class="ticketInfos">
-                    <p>Lorem ipsum dolor sit</p>
-                    <p>Mode d'obtention : e-ticket (gratuit)</p>
                 </div>
-            </div>
+                
+
+
         </div>
+        
 
 
 
         <!-- Nombre de billets -->
-        <form action="">
-            <div class="container">
-                <div class="bigMarginBottom">
-                    <h2 class="tinyMarginBottom">Nombre de billets</h2>
-                    <p class="minuscule tinyMarginTop">Les champs suivis d'un <span class="red">*</span> sont obligatoires.</p>
-                </div>
-                <div>
-                <div class="">
-        <div class="ticket-category">
-            <div class="flex">
-                <label class="tarif">Plein tarif</label>
-                <div class="counter">
-                    <button class="supprimer">-</button>
-                    <span>0</span>
-                    <button class="ajouter">+</button>
-                </div>
-            </div>
-            <!-- <div class="details">
-                <div class="person">
-                    <label class="tarif">Plein tarif 1</label>
-                    <input type="text" placeholder="Nom">
-                    <input type="text" placeholder="Prénom">
-                </div>
+        <div class="column">
+            <form action="">
+                        <div class="container">
+                            <div class="bigMarginBottom">
+                                <h2 class="tinyMarginBottom">Nombre de billets</h2>
+                                <p class="minuscule tinyMarginTop">Les champs suivis d'un <span class="red">*</span> sont obligatoires.</p>
+                            </div>
+                            <div class="">
+                    <div class="ticket-category">
+                        <div class="flex">
+                            <label class="tarif">Plein tarif</label>
+                            <div class="counter">
+                                <button class="supprimer">-</button>
+                                <span>0</span>
+                                <button class="ajouter">+</button>
+                            </div>
+                        </div>
+                        <!-- <div class="details">
+                            <div class="person">
+                                <label class="tarif">Plein tarif 1</label>
+                                <input type="text" placeholder="Nom">
+                                <input type="text" placeholder="Prénom">
+                            </div>
 
-            </div> -->
-        </div>
-        
-        <div class="ticket-category">
-            <div class="flex">
-                <label class="tarif">Enfant -16 ans</label>
-                <div class="counter">
-                    <button class="supprimer">-</button>
-                    <span>0</span>
-                    <button class="ajouter">+</button>
-                </div>
-            </div>
-            
-        </div>
-            
-        </div>
-        
-        <div class="ticket-category">
-            <div class="flex">
-                <label class="tarif">Jeune -26 ans</label>
-                <div class="counter">
-                    <button class="supprimer">-</button>
-                    <span>0</span>
-                    <button class="ajouter">+</button>
-                </div>
-            </div>
-            
-        </div>
-        
-        <div class="ticket-category">
-            <div class="flex">
-                <label class="tarif">Sénior +65 ans</label>
-                <div class="counter">
-                    <button class="supprimer">-</button>
-                    <span>0</span>
-                    <button class="ajouter">+</button>
-                </div>
-            </div>
-            
-        </div>
-                
-                <button class="validate">Valider</button>
-            </div>
-        </form>
+                        </div> -->
+                    </div>
+                    
+                    <div class="ticket-category">
+                        <div class="flex">
+                            <label class="tarif">Enfant -16 ans</label>
+                            <div class="counter">
+                                <button class="supprimer">-</button>
+                                <span>0</span>
+                                <button class="ajouter">+</button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                        
+                    </div>
+                    
+                    <div class="ticket-category">
+                        <div class="flex">
+                            <label class="tarif">Jeune -26 ans</label>
+                            <div class="counter">
+                                <button class="supprimer">-</button>
+                                <span>0</span>
+                                <button class="ajouter">+</button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                    
+                    <div class="ticket-category">
+                        <div class="flex">
+                            <label class="tarif">Sénior +65 ans</label>
+                            <div class="counter">
+                                <button class="supprimer">-</button>
+                                <span>0</span>
+                                <button class="ajouter">+</button>
+                            </div>
+                        </div>
+                        
+                    </div>
+                            
+                            <button class="validate">Valider</button>
+                        </div>
+            </form>
 
-        <div class="ticket">
-            <div class="pointilles">
-                <img src="images/img_ticket.jpg" alt="">
-                <div class="ticketInfos">
-                    <p>Lorem ipsum dolor sit</p>
-                    <p>Mode d'obtention : e-ticket (gratuit)</p>
-                </div>
-            </div>
+
+                    <div class="ticket">
+                        <div class="pointilles">
+                            <img src="images/img_ticket.jpg" alt="" class="ticketImg">
+                            <div class="ticketInfos">
+                                <p>Lorem ipsum dolor sit</p>
+                                <p>Mode d'obtention : e-ticket (gratuit)</p>
+                            </div>
+                        </div>
+                    </div>
         </div>
+       
 
 
 
 
         <!-- Coordonnées -->
-        <form action="">
-            <div class="container">
-                <div class="bigMarginBottom">
-                    <h2 class="tinyMarginBottom">Coordonnées</h2>
-                    <p class="minuscule tinyMarginTop">Les champs suivis d'un <span class="red">*</span> sont obligatoires.</p>
-                </div>
-                <div>
-                    <label for="nom">Nom <span class="red">*</span></label><br>
-                    <input type="text" id="nom" class="nom" required><br>
-                </div>
-                <div>
-                    <label for="prenom">Prénom <span class="red">*</span></label><br>
-                    <input type="text" id="prenom" class="prenom" required><br>
-                </div>
-                <div>
-                    <label for="email">Email <span class="red">*</span></label><br>
-                    <input type="email" id="email" class="email" required><br>
+        <div class="column">
+                <form action="">
+        
+                <div class="container">
+                    <div class="bigMarginBottom">
+                        <h2 class="tinyMarginBottom">Coordonnées</h2>
+                        <p class="minuscule tinyMarginTop borderResa">En procédant à la confirmation de commande, vous acceptez les conditions générales de vente et le traitement des données ci-dessous renseignées par l'agence Xploria.</p>
+                    </div>
+                    <div class="flex">
+                        <div>
+                            <label for="nom">Nom <span class="red">*</span></label><br>
+                            <input type="text" id="nom" class="nom" required><br>
+                        </div>
+                        <div>
+                            <label for="prenom">Prénom <span class="red">*</span></label><br>
+                            <input type="text" id="prenom" class="prenom" required><br>
+                        </div>
+                        <div>
+                            <label for="email">Email <span class="red">*</span></label><br>
+                            <input type="email" id="email" class="email" required><br>
+                        </div>
+                    </div>
+                    
+
+                    <button class="validate">Valider</button>
                 </div>
 
-                <button class="validate">Valider</button>
 
+            </form>
 
-        </form>
+            <div class="ticket">
+                <div class="pointilles">
+                    <img src="images/img_ticket.jpg" alt="" class="ticketImg">
+                    <div class="ticketInfos">
+                        <p>Lorem ipsum dolor sit</p>
+                        <p>Mode d'obtention : e-ticket (gratuit)</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
 
 
         </section>

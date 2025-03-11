901a57940cfc5ef8b60a57e48a3c32707df38037
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-03-11 23:24:30 +0100

    zoom au scroll avec gsap
    J'ai récupérer le code fait sur codepen avec Gsap pour le zoom au scroll.  Import de Gsap et de scrollTrigger.

--- immersion/src/scripts/bombe.js
+++ immersion/src/scripts/bombe.js
@@ -1,72 +1,72 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+// import * as THREE from 'three';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-const canvas = document.getElementById("bombe");
+// const canvas = document.getElementById("bombe");
 
-// Création de la scène
-const scene = new THREE.Scene();
+// // Création de la scène
+// const scene = new THREE.Scene();
 
-// Ajout d'une caméra
-const aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera(100, aspect, 0.1, 100);
-camera.position.set(0, 3, 8);
+// // Ajout d'une caméra
+// const aspect = window.innerWidth / window.innerHeight;
+// const camera = new THREE.PerspectiveCamera(100, aspect, 0.1, 100);
+// camera.position.set(0, 3, 8);
 
-// Utilisation du canvas pour le renderer
-const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
+// // Utilisation du canvas pour le renderer
+// const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+// renderer.setSize(window.innerWidth, window.innerHeight);
 
-// Chargement du modèle 3D 
-/////////--> cette partie la à été faite avec chatGPT
-const loader = new GLTFLoader();
-loader.load(
-    'models/bombe/bomb_shading_v005.obj',
-    (gltf) => {
-        const model = gltf.scene;
-        model.position.set(0.1, -2, -1);
-        scene.add(model);
-        console.log("Modèle chargé !");
-    },
-    (xhr) => {
-        console.log(`Chargement: ${(xhr.loaded / xhr.total) * 100}% terminé`);
-    },
-    (error) => {
-        console.error("Erreur lors du chargement du modèle :", error);
-    }
-);
-/////////
-
-
-// light 
-const ambientLight = new THREE.AmbientLight(0xffffff, 10);
-scene.add(ambientLight);
-
-const clock = new THREE.Clock();
+// // Chargement du modèle 3D 
+// /////////--> cette partie la à été faite avec chatGPT
+// const loader = new GLTFLoader();
+// loader.load(
+//     'models/bombe/bomb_shading_v005.obj',
+//     (gltf) => {
+//         const model = gltf.scene;
+//         model.position.set(0.1, -2, -1);
+//         scene.add(model);
+//         console.log("Modèle chargé !");
+//     },
+//     (xhr) => {
+//         console.log(`Chargement: ${(xhr.loaded / xhr.total) * 100}% terminé`);
+//     },
+//     (error) => {
+//         console.error("Erreur lors du chargement du modèle :", error);
+//     }
+// );
+// /////////
 
 
-//point central du code 
-const center = new THREE.Mesh(
-    new THREE.BoxGeometry(0.1, 0.1, 0.1),
-    // new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(center);
+// // light 
+// const ambientLight = new THREE.AmbientLight(0xffffff, 10);
+// scene.add(ambientLight);
 
-//orbit control
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.autorotate = true;
+// const clock = new THREE.Clock();
 
 
-// Boucle d'animation
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-}
-animate();
+// //point central du code 
+// const center = new THREE.Mesh(
+//     new THREE.BoxGeometry(0.1, 0.1, 0.1),
+//     // new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// )
+// scene.add(center);
 
-// Ajustement du canvas si la fenêtre est redimensionnée
-window.addEventListener("resize", () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-});
+// //orbit control
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.autorotate = true;
+
+
+// // Boucle d'animation
+// function animate() {
+//     requestAnimationFrame(animate);
+//     renderer.render(scene, camera);
+// }
+// animate();
+
+// // Ajustement du canvas si la fenêtre est redimensionnée
+// window.addEventListener("resize", () => {
+//     camera.aspect = window.innerWidth / window.innerHeight;
+//     camera.updateProjectionMatrix();
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+// });
--- immersion/src/scripts/index.js
+++ immersion/src/scripts/index.js
@@ -1,72 +1,42 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-
-const canvas = document.getElementById("tv");
-
-// Création de la scène
-const scene = new THREE.Scene();
-
-// Ajout d'une caméra
-const aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera(100, aspect, 0.1, 100);
-camera.position.set(0, 3, 8);
-
-// Utilisation du canvas pour le renderer
-const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-// Chargement du modèle 3D 
-/////////--> cette partie la à été faite avec chatGPT
-const loader = new GLTFLoader();
-loader.load(
-    'models/retro_television/scene.gltf',
-    (gltf) => {
-        const model = gltf.scene;
-        model.position.set(0.1, -2, -1);
-        scene.add(model);
-        console.log("Modèle chargé !");
-    },
-    (xhr) => {
-        console.log(`Chargement: ${(xhr.loaded / xhr.total) * 100}% terminé`);
-    },
-    (error) => {
-        console.error("Erreur lors du chargement du modèle :", error);
-    }
-);
-/////////
+//zoom au scroll gsap
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 
-// light 
-const ambientLight = new THREE.AmbientLight(0xffffff, 10);
-scene.add(ambientLight);
+const {innerHeight} = window;
+const maxScale = 2;
+const tvButton = document.querySelector("#tv-button");
 
-const clock = new THREE.Clock();
+if (tvButton) {
+    gsap.to("#zoom-in", {
+        scale: maxScale,
+        stagger: 0.25,
+        duration: 1,
+        scrollTrigger: {
+            pin: true,
+            end: `+=${innerHeight * 1.3}`,
+            scrub: 3,
 
-
-//point central du code 
-const center = new THREE.Mesh(
-    new THREE.BoxGeometry(0.1, 0.1, 0.1),
-    // new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(center);
-
-//orbit control
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.autorotate = true;
-
-
-// Boucle d'animation
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+            //pour l'apparision du btn de la tv
+            onUpdate: (self) => {
+                if (self.progress >= 0.5) {
+                    tvButton.classList.add("visible");
+                    console.log("visible");
+                } else {
+                    tvButton.classList.remove("visible");
+                    console.log("caché");
+                }
+            }
+        }
+    });
 }
-animate();
 
-// Ajustement du canvas si la fenêtre est redimensionnée
-window.addEventListener("resize", () => {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
+
+
+
+document.querySelector("#tv-button").addEventListener("click", () => {
+    alert("tesssstttt");
 });
+
+

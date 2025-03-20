5619b8dabb664e3d5d528590c4dc802c804c68f7
Andréa.C <155273473+Andie4@users.noreply.github.com>
2025-03-21 00:32:38 +0100

    Ajout du lancement de la vidéo dans l'écran de la salle bombe

--- immersion/src/scripts/salleBombe.js
+++ immersion/src/scripts/salleBombe.js
@@ -2,61 +2,43 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
-
-const mouse = new THREE.Vector2(1,1);
-document.addEventListener('mousemove', onMouseMove , false);
-
-function onMouseMove(event) {
-    event.preventDefault();
-    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
-    raycaster.setFromCamera(mouse, camera);
-};
-
 // scene
 const scene = new THREE.Scene();
+scene.position.set(0,1.3,1.8)
+scene.rotateY(11);
 
 // lumieres
-const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+const ambientLight = new THREE.AmbientLight(0xffffff, 20);
 scene.add(ambientLight);
 
-const color = new THREE.Color().setRGB( 255, 200, 100);
-const light = new THREE.DirectionalLight(color, 0.2);
-light.position.set(1, -8, 1);
+const light = new THREE.DirectionalLight(0xffffff, 19);
+light.position.set(0, -8, 1);
 scene.add(light);
-
-
-// resize.js
-
- const onResize = ()=>{
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize( window.innerWidth, window.innerHeight );
-}
-window.addEventListener('resize', onResize);
 
 
 // Configuration de la caméra
 const aspect = window.innerWidth / window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 5000);
-camera.position.set(0, -0.5, 1.5);
-camera.lookAt(scene.position);
-
+camera.position.set(0, 1.5, 2);
 
 // Configuration du renderer
-const renderer = new THREE.WebGLRenderer({
-    antialias: true
-});
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-
 //orbit control
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.autorotate = true;
 
-//////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////
+
+let textureVideo = null;
+const video = document.getElementById('video');
+video.muted = true;
+
+textureVideo = new THREE.VideoTexture(video);
+textureVideo.minFilter = THREE.LinearFilter;    
+textureVideo.magFilter = THREE.LinearFilter;    
+textureVideo.format = THREE.RGBFormat;        
 
 // Chargement du modèle 3D 
 const loader = new GLTFLoader();
@@ -64,47 +46,52 @@ loader.load(
     'models/salleBombe.glb',
     (gltf) => {
         const model = gltf.scene;
-        model.position.set(0, -0.0008, 0);
-        model.rotateY(5);
         scene.add(model);
-        console.log("Modèle chargé");
+        console.log("✅ Modèle chargé");
+
+        //objet 3d avec le nom ecrantv
+        const tvScreen = model.getObjectByName("ecranTV");
+        if (tvScreen) {
+            tvScreen.material = new THREE.MeshBasicMaterial({ 
+                map: textureVideo, 
+                side: THREE.DoubleSide,
+                toneMapped: false
+            });
+        } else {
+            // console.eror("Erreur pour trouver l'obj 3D ecrantv");
+        }
     },
-    (xhr) => {
-        console.log(`Chargement terminé`);
-    },
-    (error) => {
-        console.error("Erreur lors du chargement du modèle :", error);
-    }
+    undefined,
+    (error) => console.error("Erreur affiche du model :", error)
 );
 
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
 
-// mettre le son sur pause 
-window.onload = function() {
-    const buttonMute = document.getElementById("buttonDemute");
-    const buttonDemute = document.getElementById("buttonMute");
-    
-    const audio = document.getElementById("audio");
-    buttonMute.onclick = function()
-		{
-			audio.muted=true;
-			buttonMute.style.display="none";
-			buttonDemute.style.display="inline-block";
-		};
-    buttonDemute.onclick = function()
-    	{
-    		audio.muted=false;
-			buttonMute.style.display="inline-block";
-			buttonDemute.style.display="none";
-    	};
-};
+document.addEventListener("mousedown", (event) => {
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    raycaster.setFromCamera(mouse, camera);
 
+    const intersects = raycaster.intersectObjects(scene.children, true);
+    if (intersects.length > 0) {
+        let clickedObject = intersects[0].object;
+        console.log(`objets: ${clickedObject.name}`);
+
+        if (clickedObject.name === "ecranTV") {
+                video.play();
+                console.log("video en cours");
+                
+        }
+    }
+});
+ 
 
 ///////////////////////////////////////////////////////////////////////
-// Rendu de la scène
 function animate() {
     requestAnimationFrame(animate);
-
+    controls.update();
+    textureVideo.needsUpdate = true;
     renderer.render(scene, camera);
 }
-
 animate();

5e8ee6376e56a9ccc9dd898fdcff382abdf6694e
Meylie <emilie.desgranges78@gmail.com>
2025-03-21 15:11:31 +0100

    remove React style

--- back-office/src/App.jsx
+++ back-office/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useEffect } from 'react';
-import './styles/App.css'
 
 import { Home } from './pages/home'
 import { Profil } from './pages/profil'

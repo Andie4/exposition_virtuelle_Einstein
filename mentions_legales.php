b985aa291f1c933e2eeeb2150294b8107df17640
Meylie <emilie.desgranges78@gmail.com>
2025-03-23 21:31:29 +0100

    texte
    Mentions légales et infos pratiques

--- mentions_legales.php
+++ mentions_legales.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,8 +8,9 @@
     <link rel="stylesheet" href="styles/mentions_legales.css">
     <title>Mention Légales - Albert, ayez sa vision</title>
 </head>
+
 <body>
-<?php include 'nav.php'; ?>
+    <?php include 'nav.php'; ?>
 
 
     <section>
@@ -19,33 +21,55 @@
         </div>
         <div class="bigMarginBottom">
             <h2 class="tinyMarginBottom">Contributeurs : </h2>
-            <p>Auteurs : Alyssa Karahan, Konan-Hoël Lesault, Emilie Guerrier, Mélissa Cumur, Emilie Desgranges, Andréa Caneval <br>
-            Design graphique : Alyssa Karahan, Konan-Hoël Lesault, Emilie Guerrier, Mélissa Cumur. <br>
-            Développement front : Andréa Caneval <br>
-            Développement back : Emilie Desgranges <br>
-            Direction artistique : Mélissa Cumur, Alyssa Karahan</p>
+            <p>Auteurs : Alyssa Karahan, Konan-Hoël Lesault, Emilie Guerrier, Mélissa Cumur, Emilie Desgranges, Andréa
+                Caneval <br>
+                Design graphique : Alyssa Karahan, Konan-Hoël Lesault, Emilie Guerrier, Mélissa Cumur. <br>
+                Développement front : Andréa Caneval <br>
+                Développement back : Emilie Desgranges <br>
+                Direction artistique : Mélissa Cumur, Alyssa Karahan</p>
         </div>
         <div class="bigMarginBottom">
             <h2 class="tinyMarginBottom">Hébergeur : </h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus eveniet nesciunt nostrum odio placeat quibusdam a nulla molestiae laudantium eligendi error dolore beatae laboriosam sint libero ex repudiandae consectetur at necessitatibus dolor, quasi quisquam aperiam. Commodi recusandae harum voluptatibus omnis praesentium, tempora, nostrum expedita fugit eligendi atque beatae. Totam, nisi.</p>
+            <p>Ce site est hébergé par Hostinger International, situé à l'adresse suivante :<br>
+                Hostinger International Ltd.<br>
+                61, 47-49 Maughan Street, London, Royaume-Uni<br>
+                Le site est hébergé sur des serveurs situés en Europe (Union Européenne), ce qui garantit le respect des
+                règles de confidentialité et de sécurité des données personnelles conformément au RGPD.</p>
+        </div>
+
+        <div class="bigMarginBottom">
+            <h2 class="tinyMarginBottom">Collecte de données personnelles : </h2>
+            <p>Dans le cadre de la réservation pour l'exposition, nous collectons les informations suivantes :
+            <ul>
+                <li>Nom et prénom du responsable de la réservation</li>
+                <li>Adresse e-mail du responsable de la réservation</li>
+                <li>Date et horaire de la réservation</li>
+                <li>Nom et prénom des participants pour les billets</li>
+            </ul>
+            Ces données sont nécessaires pour le traitement de votre réservation et l'envoi des billets. Elles sont
+            uniquement utilisées dans ce cadre et ne sont en aucun cas partagées avec des tiers, sauf si la loi l'exige.
+            </p>
         </div>
         <div class="bigMarginBottom">
-            <h2 class="tinyMarginBottom">Cookies : </h2>
-            <p>Nous n'utilisons pas de cookies ou de tags pour suivre l'utilisation de notre plateforme par nos clients et comprendre leurs préférences sur le site “RelatiX”. En effet, nous n'avons pas besoin d'obtenir des informations générales sur le trafic de la Plateforme et ses interactions, d'identifier des tendances et d'obtenir des statistiques, puisque “” n'est pas un site commercial.</p>
+            <h2 class="tinyMarginBottom">Droit d'accès, de rectification et de suppression des données : </h2>
+            <p>Conformément au Règlement Général sur la Protection des Données (RGPD), vous disposez d’un droit d'accès,
+                de rectification, de suppression et d'opposition sur les données personnelles vous concernant. Pour
+                exercer ces droits, vous pouvez nous contacter à l'adresse suivante : <a
+                    href="mailto:xploria.agency@gmail.com">xploria.agency@gmail.com</a>.
+            </p>
         </div>
-        <div class="bigMarginBottom">
-            <h2 class="tinyMarginBottom">RGPD : </h2>
-            <p>Le Règlement Général sur la Protection des Données (RGPD) ne s'applique pas à ce site. </p>
-        </div>
+
         <div class="bigMarginBottom">
             <h2 class="tinyMarginBottom">Propriété Intellectuelle : </h2>
-            <p>Le contenu du site “RelatiX”, incluant les textes, graphismes, logos et données, est protégé par les lois relatives à la propriété intellectuelle et ne peut être reproduit sans autorisation. </p>
+            <p>Le contenu du site “Albert”, incluant les textes, graphismes, logos et données, est protégé par les lois
+                relatives à la propriété intellectuelle et ne peut être reproduit sans autorisation. </p>
         </div>
     </section>
 
     <?php include 'footer.php'; ?>
 
 
-    
+
 </body>
+
 </html>

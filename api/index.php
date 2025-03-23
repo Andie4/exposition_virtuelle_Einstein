31d75f32b060abdbca8aa204217ca473e667ad2f
Meylie <emilie.desgranges78@gmail.com>
2025-03-23 22:59:42 +0100

    index api routes publics

--- api/index.php
+++ api/index.php
@@ -4,5 +4,9 @@ header("Content-Type: application/json");
 echo json_encode([
     "message" => "Bienvenue sur l'API Expo_Einstein",
     "documentation" => "GET api/doc",
+    "routes public" => [
+        "POST" => ["admin_login", "resa"],
+        "GET" => ["tarif"],
+    ],
 ], JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 exit;
